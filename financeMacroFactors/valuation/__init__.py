9d31b96144dfb38638a5e3625feb554dd88be412
sankha <sankha.mukherjee@gmail.com>
2020-08-23 15:47:27 +0800

    valuation(update): The DCF method generated

--- financeMacroFactors/valuation/__init__.py
+++ financeMacroFactors/valuation/__init__.py
@@ -19,4 +19,5 @@ know its value? <https://medium.com/@sankha.mukherjee_007/sure-you-know-the-stoc
 '''
 
 from financeMacroFactors.valuation.valuationMethods import discountedFutureEarnings
+from financeMacroFactors.valuation.valuationMethods import discountedCashFlow
 
